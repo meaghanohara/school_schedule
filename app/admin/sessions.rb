a99f3ce0884d34468dea2aad3536ec1ad73b166d
Raghu Betina <raghu@firstdraft.com>
2019-12-09 04:21:05 +0000

    Whitelist columns in Session for ActiveAdmin

--- app/admin/sessions.rb
+++ app/admin/sessions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Session do
+
+ permit_params :user_id, :starts_at, :ends_at, :message_sent, :professor, :location
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
