e6901197dc7aaddded2dab8da69e1e2e948c67b3
Raghu Betina <raghu@firstdraft.com>
2019-12-09 04:21:05 +0000

    Only session user can access show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  before_action :current_user_must_be_session_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_session_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_session_user
     session = Session.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
